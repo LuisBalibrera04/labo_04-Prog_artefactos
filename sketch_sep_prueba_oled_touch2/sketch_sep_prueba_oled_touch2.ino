4dbe09c939e327d5107b36b8f3230084e197b868
LuisBalibrera04 <160180687+LuisBalibrera04@users.noreply.github.com>
2026-09-25 17:32:24 -0600

    Add files via upload

---
+++ sketch_sep_prueba_oled_touch2/sketch_sep_prueba_oled_touch2.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define OLED_ADDRESS 0x3C
+
+Adafruit_SH1106G display(
+  SCREEN_WIDTH,
+  SCREEN_HEIGHT,
+  &Wire,
+  OLED_RESET
+);
+
+
+const int pinBoton1 = 4;   
+const int pinBoton2 = 15;  
+
+
+const int limiteToque = 700;
+
+void setup() {
+  Serial.begin(115200);
+
+  Wire.begin(21, 22);
+
+  if (!display.begin(OLED_ADDRESS, true)) {
+    Serial.println("Error al iniciar la pantalla OLED");
+    while (1);
+  }
+
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);
+}
+
+void loop() {
+  int lectura1 = touchRead(pinBoton1);
+  int lectura2 = touchRead(pinBoton2);
+
+  Serial.print("Boton 1: ");
+  Serial.print(lectura1);
+  Serial.print(" | Boton 2: ");
+  Serial.println(lectura2);
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setCursor(26, 5);
+  display.println("ESP32 TOUCH");
+
+ 
+  if (lectura1 < limiteToque) {
+    display.setCursor(12, 28);
+    display.println("Boton 1 activado!");
+    display.fillCircle(105, 52, 5, SH110X_WHITE);
+  }
+  
+  else if (lectura2 < limiteToque) {
+    display.setCursor(12, 28);
+    display.println("Boton 2 activado!");
+    display.fillCircle(64, 52, 5, SH110X_WHITE);
+  }
+  
+  else {
+    display.setCursor(32, 28);
+    display.println("Esperando...");
+    display.fillCircle(20, 52, 5, SH110X_WHITE);
+  }
+
+  display.display();
+
+  delay(100);
+}
